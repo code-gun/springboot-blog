b6ad05f375904bc42c92a492715c42759d915bdf
kks <kim51942348@gmail.com>
2024-06-13 17:23:54 +0900

    Update Commit

---
+++ src/main/java/me/kks/springboot_blog/springbootblog/Member.java
@@ -0,0 +1,20 @@
+package me.kks.springboot_blog.springbootblog;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+}
